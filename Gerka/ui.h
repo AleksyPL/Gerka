981851b7f73205ec839508ca1597c6c54bccac25
Michał Aleksandrowicz <45513444+AleksyPL@users.noreply.github.com>
2020-03-03 22:12:20 +0100

    Delete ui.h

--- Gerka/ui.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "biblioteki.h"
-#include "dwellers.h"
-#include "player.h"
-
-int ui(player gracz,barman bobby, seller handlarz, blacksmith kowal, alchemist alchemik,shaman szaman,doctor lekarz, chest krzynka);
