65fa83486df1bb8772484509909ab9494d87a930
Michał Aleksandrowicz <45513444+AleksyPL@users.noreply.github.com>
2020-03-03 22:10:57 +0100

    Delete trade.h

--- Gerka/trade.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "biblioteki.h"
-#include "dwellers.h"
-#include "trade_tab.h"
-#include "player.h"
-
-class table;
-player trade_mode(player &gracz,seller &handlarz, table &menu);
