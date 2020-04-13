370a3da5fdee8fbcd7fcf1afb4cb7f09111635e4
Michał Aleksandrowicz <45513444+AleksyPL@users.noreply.github.com>
2020-04-13 21:07:03 +0200

    Delete tawerna.h

--- Gerka/tawerna.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-//#include "biblioteki.h"
-#include "dwellers.h"
-#include "player.h"
-
-void drunked(int height, int startPoint, player& gracz);
-void robbery(int height, int startPoint, player &gracz, vector <string> message);
-void flirting(int height, int startPoint, player &gracz);
-player enterTavern(player gracz,barman bobby, chest &krzynka);
