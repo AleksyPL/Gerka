92682505dda704019875e547962f7854f574b057
Michał Aleksandrowicz <45513444+AleksyPL@users.noreply.github.com>
2020-04-13 21:00:38 +0200

    Delete exit.h

--- Gerka/exit.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Biblioteki.h"
-#include "ui.h"
-#include "save.h"
-
-void exit(player gracz, barman bobby, seller handlarz, blacksmith kowal, alchemist alchemik, shaman szaman, doctor lekarz);
